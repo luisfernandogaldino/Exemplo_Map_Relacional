5266fbf175d89bdbea6f3b7e2bed2f764a0e539c
luisfernandogaldino <133809489+luisfernandogaldino@users.noreply.github.com>
2024-02-29 16:29:10 -0300

    Add files via upload

---
+++ MapRelacional/src/main/java/com/mapRelacional/MapRelacionalApplication.java
@@ -0,0 +1,13 @@
+package com.mapRelacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapRelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapRelacionalApplication.class, args);
+	}
+
+}
---
+++ MapRelacional/src/test/java/com/mapRelacional/MapRelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapRelacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapRelacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
